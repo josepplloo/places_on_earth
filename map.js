39e0653c637ba9c5c5adce5e02e44f2f646487dc
pingo <--get>
2019-03-14 15:42:31 -0500

    Now I storage the markers in the local session

--- map.js
+++ map.js
@@ -7,15 +7,12 @@ function init() {
   tomtom.setProductInfo(env.productName, env.version);
   tomtom.key(env.APIKEY);
   tomtom.searchKey(env.APIKEY);
-
+  //init the map in Amsterdam
   map = tomtom.map('map', {
     source: 'vector',
     basePath: '/sdk'
-  }).setView([52.373154, 4.890659], 14);
+  }).setView([52.373154, 4.890659], 5);
 
-  // Adding marker to the map
-  
-  
   map.addEventListener('click', addPlace);
 
   const placesFromLocalStorage = JSON.parse(localStorage.getItem('myPlaces'));
@@ -27,19 +24,23 @@ function init() {
 }
 
 function addPlace(event) {
-  let marker = tomtom.L.marker(event.latlng, {
-    draggable: true
-  }).bindPopup(`Ma Place ${++cont}`).addTo(map);
-
-  myPlaces.push( marker );
   
-  localStorage.setItem('myPlaces', JSON.stringify(event.latlng));
+  myPlaces.push( event.latlng );
+  
+  localStorage.setItem('myPlaces', JSON.stringify(myPlaces));
 
   renderMarkers();
 }
 
 function renderMarkers() {
-
+  myPlaces.forEach(event => {
+    let title = `Ma Place ${++cont}`;
+    let marker = tomtom.L.marker(new tomtom.L.LatLng(event.lat,event.lng), {
+      title: `${title}`
+    });
+    marker.bindPopup(title).addTo(map);
+  })
+  console.log(localStorage.getItem('myPlaces'));
 }
 
 init();
